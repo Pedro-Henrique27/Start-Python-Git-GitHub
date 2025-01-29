041c6bce42e25b89bb640fdba3da84f248d88090
pedrohenriquecan <pedro.s.candido@gmail.com>
2025-01-29 11:20:52 -0300

    update

--- desafio006.py
+++ desafio006.py
@@ -1,1 +1,7 @@
+import math
+
 print('-'*7,'DESAFIO_006',7*'-')
+numero = int(input('Digite um número: '))
+print('O dobro de {} é {}'.format(numero,(numero*2)))
+print('O triplo de {} é {}'.format(numero,(numero*3)))
+print('A raiz quadrade de {} é {:.2}'.format(numero,(math.sqrt(numero))))
