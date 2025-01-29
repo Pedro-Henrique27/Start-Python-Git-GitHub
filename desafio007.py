b3f60450a1d3ce35035aded60ec1be0a336e0bc0
pedrohenriquecan <pedro.s.candido@gmail.com>
2025-01-29 14:06:08 -0300

    up

--- desafio007.py
+++ desafio007.py
@@ -1,1 +1,5 @@
 print('-'*7,'DESAFIO_007',7*'-')
+nota1=float(input('Digite a primeira nota: '))
+nota2=float(input('Digite a segunda nota: '))
+
+print('Sua média foi {:.1f}'.format((nota1+nota2)/2))
